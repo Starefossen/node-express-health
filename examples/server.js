b2965e9053f09e019a26cd4fdc6b07f98809a09a
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-06-03 08:52:51 +0200

    feat(example): add server examples

---
+++ examples/server.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const express = require('express');
+const app = module.exports = express();
+
+const healthCheck = require('../');
+
+// normal route
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// simple health check
+app.get('/simpleCheck', healthCheck.middleware);
+
+// single health check
+app.get('/healthCheck', healthCheck({
+  name: 'my service',
+  check: cb => {
+    cb(null, 'Service Ok');
+  },
+}));
+
+// multiple health checks
+app.get('/healthChecks', healthCheck([{
+  name: 'db',
+  check: cb => {
+    cb(null, 'Database Ok');
+  },
+}, {
+  name: 'cache',
+  check: cb => {
+    cb(null, 'Cache Ok');
+  },
+}]));
+
+// health check with error
+app.get('/healthCheckError', healthCheck([{
+  name: 'db',
+  check: cb => {
+    cb(null, 'Database Ok');
+  },
+}, {
+  name: 'cache',
+  check: cb => {
+    cb(new Error('Could not connect to cache'));
+  },
+}]));
+
+// handle cors errors
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  res.status(err.code).send(err.toJSON());
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000'); // eslint-disable-line no-console
+}
---
+++ test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+process.env.CORS_ALLOW_ORIGINS = 'foo.com,bar.com';
+
+const assert = require('assert');
+const request = require('supertest');
+const simpleApp = request(require('./examples/server'));
+
+describe('examples', () => {
+  describe('server', () => {
+    it('single check', done => {
+      simpleApp.get('/healthCheck')
+        .expect(200)
+        .expect({
+          code: 200,
+          message: 'Ok',
+          services: [{
+            name: "my service",
+            status: "Service Ok"
+          }]
+        }, done)
+    });
+
+    it('multiple checks', done => {
+      simpleApp.get('/healthChecks')
+        .expect(200)
+        .expect({
+          code: 200,
+          message: 'Ok',
+          services: [{
+            name: "db",
+            status: "Database Ok"
+          }, {
+            name: "cache",
+            status: "Cache Ok"
+          }]
+        }, done)
+    });
+
+    it('health check error', done => {
+      simpleApp.get('/healthCheckError')
+        .expect(500)
+        .expect({
+          code: 500,
+          message: 'Service "cache" returned error'
+        }, done)
+    });
+  });
+});
